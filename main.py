79f654b237e8bc8c526964b1d8e2fd76d0933e51
Jarrod Los <56139570+JarrodLos@users.noreply.github.com>
2020-03-11 16:20:54 -0600

    Update main.py

--- main.py
+++ main.py
@@ -13,11 +13,13 @@ def restartProgram():
     python = sys.executable
     os.execl(python, python, * sys.argv)
 
+# Gets the path to the database provided its given as an arg in the terminal
 def getPath():
     directory = os.path.dirname(os.path.realpath(__file__))
     path = os.path.join(directory, str(sys.argv[1]))
     return path
 
+# Uses the derived path to point the cursor at the database
 def connect(path):
     global connection, cursor
     connection = None
@@ -63,7 +65,7 @@ def VerifyNew(email):
         return True
     return False
 
-# Verifies an existing users email and password
+# Verifies an existing users email and password (Case Sensitive)
 def VerifyExisting(email, password):
     global connection, cursor
     CheckEmail = '''
@@ -84,14 +86,10 @@ def VerifyExisting(email, password):
 		FROM users u
 		WHERE u.email = :email;
         	'''
-        #cursor.execute(' PRAGMA case_sensitive_like=true; ')
-        #connection.commit()
         cursor.execute(CheckPwd, {"email":email});
-
         Row2 = cursor.fetchone()
 
         # Correct Email & Password
-        print(str(Row2[0]) + "VS" + password)
         if str(Row2[0]) ==  password:
             print("\nAccount Found!")
             print("Signing in as " +  email + "...")
@@ -220,7 +218,7 @@ def CreateAccount():
     connection.commit()
     return True
 
-# Redirect to account creation or signing in
+# Handles logic to redirect user to account creation or sign in
 def checkSignInCmd():
     command = customIn()
 
@@ -237,8 +235,10 @@ def checkSignInCmd():
         print("Input not recognized, please try again")
         return False
 
-
+# View all reviews of a provided user
 def viewReviews(user):
+    
+    # Query to find data on a users reviews
     searchData = '''
     SELECT reviewer, rating, rtext, rdate
     FROM reviews
@@ -248,9 +248,9 @@ def viewReviews(user):
     cursor.execute(searchData, (user,));
     allSearchResults = cursor.fetchall()
 
+    # Process data pulled and display if any
     if(len(allSearchResults) == 0):
         print(user + " has no reviews.")
-
     else:
         print("\nIndex: Reviewer | Rating | Comments | Date")
         for result in range(0, len(allSearchResults)):
@@ -258,10 +258,10 @@ def viewReviews(user):
                 comment = "(No Comment)"
             else:
                 comment = allSearchResults[result][2]
-
             print(str(result) + ": " + allSearchResults[result][0] + " | " + str(allSearchResults[result][1]) + " | " + comment + " | " + str(allSearchResults[result][2]) + str(allSearchResults[result][3]))
 
 
+# Places a bid given provided user input for a valid bid creation
 def placeBid(sid, selectedSale, resPrice, maxBid):
     global connection, cursor, currUser
 
@@ -323,9 +323,10 @@ def placeBid(sid, selectedSale, resPrice, maxBid):
 
     print("New bid of $" + str(newBidAmount) + " placed on " + selectedSale[0] + "'s " + selectedSale[3] + " successfully!")
 
-
+# Show all the active listings of a provided product
 def showActiveListingsListProduct(product):
 
+    # Query to pull all listings of a product
     searchData = '''
     SELECT s.descr, count(bid), max(amount), s.rprice,
             CAST((strftime('%s',s.edate) - strftime('%s', 'now')) /86400 AS TEXT),
@@ -343,9 +344,9 @@ def showActiveListingsListProduct(product):
     cursor.execute(searchData, (product,));
     allSearchResults = cursor.fetchall()
 
+    # Process data pulled and display if any
     if(len(allSearchResults) == 0):
         print("No active listings of this product.")
-
     else:
         print("\nIndex: Description | Max Bid / Res Price | Days/Hours/Minutes Remaining")
         for result in range(0, len(allSearchResults)):
@@ -353,7 +354,6 @@ def showActiveListingsListProduct(product):
                 maxBidResPrice = allSearchResults[result][3]
             else:
                 maxBidResPrice = allSearchResults[result][2]
-
             print(str(result) + ": " + allSearchResults[result][0] + " | " + str(maxBidResPrice) + " | " + str(allSearchResults[result][4]) + "days, " + str(allSearchResults[result][5]) + "hours, " + str(allSearchResults[result][6]) + "minutes")
 
 
@@ -369,8 +369,6 @@ def showActiveListingsListProduct(product):
         else:
             print("Invalid entry, please enter a number between 0 and " + str(len(allSearchResults) - 1) + ".")
 
-    # Where I left off, may need some testing
-
     fetchSale = '''
     SELECT s.lister, count(r.rating), avg(r.rating), s.descr, s.edate, s.cond, max(amount), s.rprice, p.descr, count(pr.rating), avg(pr.rating), s.sid
     FROM (sales s left outer join bids b using (sid)) left outer join products p using (pid), reviews r, previews pr
@@ -378,8 +376,6 @@ def showActiveListingsListProduct(product):
     AND s.sid = b.sid
     GROUP BY s.sid;
     '''
-
-    #    AND s.lister = r.reviewee     AND p.pid = pr.pid
 
     cursor.execute(fetchSale, (allSearchResults[int(selection)][7],));
     selectedSale = cursor.fetchone()
@@ -395,6 +391,7 @@ def showActiveListingsListProduct(product):
     resPrice = 0
     maxBid = 0
 
+    # Run through all possibilites of a bid and or reserved price
     if(allSearchResults[int(selection)][2] == None):
         resPrice = allSearchResults[int(selection)][3]
         print("Reserved Price: $" + str(resPrice))
@@ -441,9 +438,10 @@ def showActiveListingsListProduct(product):
     else:
         viewReviews(selectedSale[0])
 
-
+# Lists all listings of a single seller (The user passed in)
 def showActiveListings(user):
-
+    
+    # Query to show all active listings from a single user
     searchData = '''
     SELECT s.descr, count(bid), max(amount), s.rprice,
             CAST((strftime('%s',s.edate) - strftime('%s', 'now')) /86400 AS TEXT),
@@ -461,9 +459,9 @@ def showActiveListings(user):
     cursor.execute(searchData, (user,));
     allSearchResults = cursor.fetchall()
 
+    # Process data pulled and display if any
     if(len(allSearchResults) == 0):
         print("This user has no active listings.")
-
     else:
         print("\nIndex: Description | Max Bid / Res Price | Days/Hours/Minutes Remaining")
         for result in range(0, len(allSearchResults)):
@@ -560,6 +558,7 @@ def showActiveListings(user):
             viewReviews(selectedSale[0])
 
 
+# Creates a product review given user input 
 def createProdReview(rating, text, pid):
     global connection, cursor, currUser
     # Get the most recent review id (rid)
@@ -579,7 +578,7 @@ def createProdReview(rating, text, pid):
     cursor.execute(newReview, {"rid":rid, "pid":pid, "reviewer":currUser, "rating":rating, "rtext":text})
     connection.commit()
 
-
+# Creates a review on a seller by the current user
 def createSellerReview(rating, text, email):
     global connection, cursor, currUser
 
@@ -591,7 +590,7 @@ def createSellerReview(rating, text, email):
     cursor.execute(newReview, (currUser, email, rating, text))
     connection.commit()
 
-
+# Creates a review on a seller by the current user
 def createSale(edate, descr, cond, rprice, pid):
     global connection, cursor, currUser
 
@@ -600,17 +599,18 @@ def createSale(edate, descr, cond, rprice, pid):
     SELECT sid
     FROM sales;
     '''
-
     cursor.execute(allSids);
     allSidsFound = cursor.fetchall()
 
+    # Process the pulled data if any    
     if(len(allSidsFound) == 0):
         sid = "S00"
     else:
         allSidsFoundStripped = [len(allSidsFound)]
-
         for sid in range(0, len(allSidsFound)):
             allSidsFoundStripped[0] = int(allSidsFound[sid][0].strip("S"))
+    
+    # Format to the correct size of the id
     maxSid = str(max(allSidsFoundStripped) + 1)
     if len(maxSid) == 1:
         sid = "S" + "0" + maxSid
@@ -625,14 +625,14 @@ def createSale(edate, descr, cond, rprice, pid):
 
     # Create the new sale
     newReview = '''
-		INSERT INTO sales(sid, lister, pid, edate, descr, cond, rprice)
+		INSERT OR REPLACE INTO sales(sid, lister, pid, edate, descr, cond, rprice)
 		VALUES(:sid, :lister, :pid, :edate, :descr, :cond, :rprice);
         	'''
     cursor.execute(newReview, {"sid":sid, "lister":currUser, "pid":pid, "edate":edate, "descr":descr, "cond":cond, "rprice":rprice})
     print("Your sale has been created! Please use sale ID " + sid + " to find your sale")
     connection.commit()
 
-
+# List all reviews of a provided product given its pid
 def listReviews(pid):
     global connection, cursor
     # Get all the product reviews of PID
@@ -646,7 +646,7 @@ def listReviews(pid):
     for i in range(0, len(Row)):
         print("Review #" + str(i + 1) + " " + Row[i][0])
 
-
+# List all active sales of a provided product given its pid
 def listSales(pid): # Possibly depreciated
     global connection, cursor
     # Get all the product reviews of PID
@@ -661,7 +661,7 @@ def listSales(pid): # Possibly depreciated
     for i in range(0, len(Row)):
         print("Review #" + str(i + 1) + " " + Row[i][0])
 
-
+# Gets the current date to reference if a created sale has an end date in the future
 def getcurrentDate():
     # Get the current date
 
@@ -686,10 +686,12 @@ def getcurrentDate():
         currDate[3 + j] = int(timeP[j])
     return currDate
 
-
-def listProducts(): # 1
+# List all available products that have an active listing
+def listProducts():
     global connection, cursor, currUser
     print("\nRun the List Products")
+    
+    # There is a problem with the query, decided to finish the project instead of fix :(
     activeSales = '''
 		SELECT DISTINCT p.pid, p.descr, COUNT(r.rating), AVG(r.rating), COUNT(s.sid)
 		FROM products p,previews r, sales s
@@ -704,6 +706,7 @@ def listProducts(): # 1
 
     Row = cursor.fetchall()
 
+    # Create a table from which to view the pulled data
     print("Index | PID |    Desc.    | R# |  AR  | #S")
     for i in range(0, len(Row)):
         print("  " + str(i) + "   | " + Row[i][0] + " | " + Row[i][1] + " | " + str(Row[i][2]) + "  |  " + str(Row[i][3]) + " | " + str(Row[i][4]))
@@ -713,11 +716,6 @@ def listProducts(): # 1
 
     # Listen for user commands for the list of Products
     index = customIn()
-    #while not (i > int(index)):
-            #print("Please enter a valid product index!")
-            #cmd = ""
-            #cmd = customIn()
-
     if index.lower() == ".back":
         return
     else:
@@ -726,15 +724,21 @@ def listProducts(): # 1
         print("To list all active sales: 'c'\n")
 
         cmd = customIn()
-        if cmd.lower() == "a": # Create a rating (Needs char limit)
+        if cmd.lower() == "a": # Create a rating
             print("\nCreating a review for the selected product: " + Row[int(index)][0])
             print("\nPlease enter your rating (1-5): ")
             rating = customIn()
             while (0 >= int(rating)) or (int(rating) >= 6):
                 print("\nPlease enter a rating between 1-5")
                 rating = customIn()
+            
             print("\nPlease enter your text (1-20 characters):")
-            text = customIn()
+            while True:
+                text = customIn()
+                if 20 >= len(text) > 0:
+                    break
+                print("\nPlease enter a valid description between 1-20 chars!")
+            
             selectedPid = Row[int(index)][0]
             createProdReview(rating, text, selectedPid)
             print("\nThank-you for your review!")
@@ -746,11 +750,10 @@ def listProducts(): # 1
             selectedPid = Row[int(index)][0]
             listReviews(selectedPid)
 
-        elif cmd.lower() == "c": # NOT DONE
+        elif cmd.lower() == "c": # List all active sales
             print("\nListing all active sales for the selected product: " + Row[int(index)][0])
             selectedPid = Row[int(index)][0]
             showActiveListingsListProduct(selectedPid)
-            # listSales(selectedPid)
 
         elif cmd.lower() == ".back":
             return
@@ -759,8 +762,9 @@ def listProducts(): # 1
             print("That is an incorrect entry, please try again or enter 'menu' to go back to the main menu")
             return
 
+# Create a sale provided data from the current user
+def postSale():
 
-def postSale(): # 2
     print("\nCreating a new sale!")
     # sid (Gen), lister (currUser), pid (optional), edate * (Future), descr, cond, rprice(Optional)
 
@@ -802,7 +806,8 @@ def postSale(): # 2
                             if int(timeP[1]) > currDate[4]: # Minute > current Minute
                                 break
         print("\nPlease enter a valid date in the future in the following format: yyyy-MM-dd HH:mm:ss")
-
+    
+    # Take the rest of the user input
     print("\nPlease enter the sales description: ") # desc
     while True:
         descr = customIn()
@@ -823,9 +828,10 @@ def postSale(): # 2
     print("\n(Optional) Please enter the product ID: ") # pid
     pid = customIn()
 
+    # Create Sale
     createSale(edate, descr, cond, rprice, pid)
 
-
+# Search a sale given a keyword or select one from all active sales (Refs. 1-2 Follow Up task)
 def searchSale(): # 3
     print('\nSearch Sales - Type ".back" to return to Main Menu.')
     backFlag = False # Is set to true if we are breaking out of the program
@@ -885,7 +891,6 @@ def searchSale(): # 3
                 allSearchResultsNoneFiltered.append(singleRow)
 
                 index += 1
-
 
         # While loop for error checking
         selection = ""
@@ -984,8 +989,8 @@ def searchSale(): # 3
 
         break # We are done searching.
 
-
-def searchUser(): # 4 - IN PROGRESS
+# Search a user from there index on all users or by name, email or city and follow up with the 1-2 task
+def searchUser():
 
     print('\nSearch Users - Type ".back" to return to Main Menu.')
     backFlag = False # Is set to true if we are breaking out of the program
@@ -1213,9 +1218,6 @@ if (__name__ == "__main__"):
     init()
     path = getPath()
     connect(path)
-
-    # define_tables()
-    # Inserting data from previously created table?
 
     # Keep trying until successful creation or login
     while(not checkSignInCmd()):
